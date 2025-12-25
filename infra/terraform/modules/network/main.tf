15d693d449c0e6c1c164f8dfc077c697f89689d3
Vishal Chaphekar <vychaphekar@evanyaconsulting.com>
2025-12-24 23:49:35 -0600

    Fixed network issues

--- infra/terraform/modules/network/main.tf
+++ infra/terraform/modules/network/main.tf
@@ -1,5 +1,9 @@
 data "aws_region" "current" {}
 
+data "aws_availability_zones" "available" {
+  state = "available"
+}
+
 resource "aws_vpc" "this" {
   cidr_block           = var.vpc_cidr
   enable_dns_support   = true
@@ -10,7 +14,7 @@ resource "aws_vpc" "this" {
 resource "aws_subnet" "private_a" {
   vpc_id            = aws_vpc.this.id
   cidr_block        = "10.20.1.0/24"
-  availability_zone = "${data.aws_availability_zones}a"
+  availability_zone = data.aws_availability_zones.available.names[0]
   tags = { Name = "${var.name}-priv-a" }
 }
 
@@ -30,6 +34,6 @@ resource "aws_security_group" "apigw_vpc_link" {
 resource "aws_subnet" "private_b" {
   vpc_id            = aws_vpc.this.id
   cidr_block        = "10.20.2.0/24"
-  availability_zone = "${data.aws_availability_zones}b"
+  availability_zone = data.aws_availability_zones.available.names[1]
   tags = { Name = "${var.name}-priv-b" }
 }
